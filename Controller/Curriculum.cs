b74605e3c82ad605eba3bedf9b23954aed659bf4
cristian <cfejaramillo@poligran.edu.co>
2024-08-15 13:47:53 -0500

    Portafilio personal

---
+++ Controller/Curriculum.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portafolio.Controller
+{
+    public class Curriculum : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return RedirectToPage("Curriculum");
+        }
+    }
+}
